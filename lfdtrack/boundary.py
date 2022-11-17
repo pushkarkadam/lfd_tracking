d378b088748b5fff0e4a7779b9ab759c0ebd6369
Pushkar Kadam <pushkarkadam17@outlook.com>
2022-11-17 12:13:49 +1100

    neighbor_8 detection added

--- lfdtrack/boundary.py
+++ lfdtrack/boundary.py
@@ -36,3 +36,67 @@ def upper_left_value(I, value=1):
             if I[row][col] == value:
                 b0 = (row, col)
                 return b0
+
+def neighbors_8(I, pixel_coords, background_point):
+    """Returns a dictionary of pixel co-ordinates and their respective values.
+    
+    Orders the neighbors in sequence.
+    The sequence starts with the background point.
+    The algorithm is a part of Moore boundary tracing algorithm.
+    Where the n1 must start from C (``backgorund_point``).
+    The algorithm uses clockwise checking.
+    
+    Parameters
+    ----------
+    I: np.ndarray
+        A numpy array.
+        This array represents the binary image.
+    pixel_coords: tuple
+        The central pixel co-ordinates.
+        Represented as b in the algorithm.
+    background_point: tuple
+        The co-ordinate of the point that traces the boundary.
+        Represented as c in the algorithm
+        
+        
+    Returns
+    -------
+    list
+        A list of all the 8 neighbor co-ordinates in a sequence followed
+        by the background point.
+        
+    Examples
+    --------
+    >>> from lfdtrack import *
+    >>> I = np.array([[1,2,3], [4,4,5]])
+    >>> I = np.pad(I, 1)
+    >>> neighbors_8(bar, (1,1), (1,2))
+    [(1, 2), (2, 2), (2, 1), (2, 0), (1, 0), (0, 0), (0, 1), (0, 2)]
+    
+    """
+    
+    i, j = pixel_coords
+    c = background_point
+    
+    n1 = (i-1, j-1)
+    n2 = (i-1, j)
+    n3 = (i-1, j+1)
+    n4 = (i, j+1)
+    n5 = (i+1, j+1)
+    n6 = (i+1, j)
+    n7 = (i+1, j-1)
+    n8 = (i, j-1)
+    
+    neighbors = [n1, n2, n3, n4, n5, n6, n7, n8]
+    
+    # Finding the location of the C element
+    c_loc = neighbors.index(c)
+    
+    # slicing the list
+    after = neighbors[:c_loc]
+    before = neighbors[c_loc:]
+    
+    # Matching the list
+    sequence = before + after
+    
+    return sequence

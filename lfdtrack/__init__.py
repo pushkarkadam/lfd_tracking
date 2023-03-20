7650ffeb92485a23e8d9746baded6af73c171f7e
Pushkar Kadam <pushkarkadam17@outlook.com>
2023-03-20 12:43:01 +1100

    weldment roi function added

--- lfdtrack/__init__.py
+++ lfdtrack/__init__.py
@@ -9,3 +9,7 @@ from .capture import *
 import lfdtrack.fingertrack
 from lfdtrack.fingertrack import *
 from .fingertrack import *
+
+import lfdtrack.roi 
+from lfdtrack.roi import *
+from .roi import *

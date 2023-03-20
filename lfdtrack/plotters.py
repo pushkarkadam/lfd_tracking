ea724f6ca083e07e5f0ba760bf0d9ab968c62edd
Pushkar Kadam <pushkarkadam17@outlook.com>
2023-03-21 10:13:10 +1100

    path planner function added

--- lfdtrack/plotters.py
+++ lfdtrack/plotters.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+from mpl_point_clicker import clicker
 
 
 def point_plotter(image_path, filename=''):
@@ -49,4 +50,54 @@ def point_plotter(image_path, filename=''):
         with open(filename, 'wb') as f:
             pickle.dump(points, f)
 
+    return points
+
+def path_planner(image_path, filename=''):
+    """Returns a dictionary of x and y co-ordinates.
+
+    Parameters
+    ----------
+    image_path: str
+        Path to the image.
+
+    filename: str, default ``''``
+        Path to store the points data.
+        Use the ``.pkl`` extension.
+        Usage: ``filename="~/path/to/filename.pkl"``
+
+    Returns
+    -------
+    dict
+        A dictionary of x and y values of the points.
+
+    """
+
+    fig, ax = plt.subplots(constrained_layout=True)
+
+    # Reading image from the path
+    image = plt.imread(image_path)
+
+    ax.imshow(image, cmap="gray")
+    klicker = clicker(
+    ax,
+    ["points"],
+    markers=["o"],
+    linestyle= "--",
+    colors=['r']
+    )
+
+    plt.show()
+
+    coords = klicker.get_positions()
+
+    points = {'x': [], 'y': []}
+
+    for coord in list(coords['points']):
+        points['x'].append(np.array(coord)[0])
+        points['y'].append(np.array(coord)[1])
+
+    if filename:
+        with open(filename, 'wb') as f:
+            pickle.dump(points, f)
+        
     return points
